62355d04551aa1073923cb166a2e1d21e7747182
Gabriel Sroka <gabrielsroka@gmail.com>
2022-01-31 15:48:11 -0800

    Update r.py

--- r.py
+++ r.py
@@ -10,7 +10,7 @@ def url(url, **kwargs):
     return url + '?' + urllib.parse.urlencode(kwargs)
 
 def r(method, url, json=None):
-    """Returns HTTPResponse object (including res.reason, .status, .headers) and also .json, .next_url."""
+    """Returns HTTPResponse object (including res.reason, .status, .headers) and also .json."""
     _headers = headers.copy()
     if json:
         data = json_.dumps(json, separators=(',', ':')).encode()
@@ -25,6 +25,7 @@ def r(method, url, json=None):
     return res
 
 def get(url):
+    """Returns HTTPResponse object (including res.reason, .status, .headers) and also .json, .next_url."""
     res = r('GET', url)
     links = [link for link in res.headers.get_all('link', []) if 'rel="next"' in link]
     res.next_url = re.search('<(.*)>', links[0]).group(1) if links else None
