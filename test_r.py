4d389d6e41b1540277d5b7739d19db8c004ba49a
Gabriel Sroka <gabrielsroka@gmail.com>
2022-12-07 09:15:04 -0800

    Update test_r.py

--- test_r.py
+++ test_r.py
@@ -1,40 +1,29 @@
 import r
-import json
+import os
 
 # Set these:
-with open('okta.yaml') as f: # in JSON format: {"okta":{"client":{"orgUrl":"...","token":"..."}}}
-    config = json.load(f)['okta']['client']
-
-base_url = config['orgUrl'] # eg 'https://gsroka.oktapreview.com'
-token = config['token']
-
+org_url = os.environ['OKTA_CLIENT_ORGURL']
+token = os.environ['OKTA_CLIENT_TOKEN']
 group_id = '...'
 
-r.headers.update({
-    'Authorization': 'SSWS ' + token,
-    'Accept': 'application/json'
-})
+r.headers['authorization'] = 'SSWS ' + token
 
-res = r.get(f'{base_url}/api/v1/users/me')
+res = r.get(f'{org_url}/api/v1/users/me')
 me = res.json
 user_id = me['id']
 print(me['profile']['login'], me['profile']['title'], res.headers['x-rate-limit-remaining'])
 
 # Pagination.
-url = r.url(f'{base_url}/api/v1/users', filter='profile.lastName eq "Doe"', limit=2)
-while url:
-    res = r.get(url)
-    print(len(res.json), url, res.headers['x-rate-limit-remaining'])
-    for user in res.json:
-        print(user['profile']['login'])
-    url = res.next_url
+url = r.url(f'{org_url}/api/v1/users', filter='profile.lastName eq "Doe"', limit=2)
+for user in r.get_objects(url):
+    print(user['profile']['login'])
 
-res = r.post(f'{base_url}/api/v1/users/{user_id}', {'profile': {'title': 'technoking'}})
-me = res.json
-print(me['profile']['title'], res.headers['x-rate-limit-remaining'])
+# res = r.post(f'{org_url}/api/v1/users/{user_id}', {'profile': {'title': 'admin'}})
+# me = res.json
+# print(me['profile']['title'], res.headers['x-rate-limit-remaining'])
 
-res = r.put(f'{base_url}/api/v1/groups/{group_id}/users/{user_id}')
-print(res.headers['x-rate-limit-remaining'])
+# res = r.put(f'{org_url}/api/v1/groups/{group_id}/users/{user_id}')
+# print(res.headers['x-rate-limit-remaining'])
 
-res = r.delete(f'{base_url}/api/v1/groups/{group_id}/users/{user_id}')
-print(res.headers['x-rate-limit-remaining'])
+# res = r.delete(f'{org_url}/api/v1/groups/{group_id}/users/{user_id}')
+# print(res.headers['x-rate-limit-remaining'])
