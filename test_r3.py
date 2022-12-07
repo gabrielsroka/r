f390d19223bc55be4c1b7ea19572606f348b4cd3
Gabriel Sroka <gabrielsroka@gmail.com>
2022-12-07 09:12:25 -0800

    Update test_r3.py

--- test_r3.py
+++ test_r3.py
@@ -1,10 +1,9 @@
 import r3
 import os
 
-# Set these secrets:
-base_url = os.environ['base_url']
-token = os.environ['token']
-
+# Set these:
+org_url = os.environ['OKTA_CLIENT_ORGURL']
+token = os.environ['OKTA_CLIENT_TOKEN']
 group_id = '...'
 
 r3.headers.update({
@@ -12,26 +11,22 @@ r3.headers.update({
     'Accept': 'application/json'
 })
 
-res = r3.get(f'{base_url}/api/v1/users/me')
+res = r3.get(f'{org_url}/api/v1/users/me')
 me = res.json
 user_id = me['id']
 print(me['profile']['login'], me['profile']['title'], res.headers['x-rate-limit-remaining'])
 
 # Pagination.
-url = r3.url(f'{base_url}/api/v1/users', filter='profile.lastName eq "Doe"', limit=2)
-while url:
-    res = r3.get(url)
-    for user in res.json:
-        print(user['profile']['login'])
-    url = res.next_url
-    # print(len(res.json), res.headers['x-rate-limit-remaining'])
+url = r3.url(f'{org_url}/api/v1/users', filter='profile.lastName eq "Doe"', limit=2)
+for user in r3.get_objects(url):
+    print(user['profile']['login'])
 
-res = r3.post(f'{base_url}/api/v1/users/{user_id}', {'profile': {'title': 'admin'}})
-me = res.json
-print(me['profile']['title'], res.headers['x-rate-limit-remaining'])
+# res = r3.post(f'{org_url}/api/v1/users/{user_id}', {'profile': {'title': 'admin'}})
+# me = res.json
+# print(me['profile']['title'], res.headers['x-rate-limit-remaining'])
 
-res = r3.put(f'{base_url}/api/v1/groups/{group_id}/users/{user_id}')
-print(res.headers['x-rate-limit-remaining'])
+# res = r3.put(f'{org_url}/api/v1/groups/{group_id}/users/{user_id}')
+# print(res.headers['x-rate-limit-remaining'])
 
-res = r3.delete(f'{base_url}/api/v1/groups/{group_id}/users/{user_id}')
-print(res.headers['x-rate-limit-remaining'])
+# res = r3.delete(f'{org_url}/api/v1/groups/{group_id}/users/{user_id}')
+# print(res.headers['x-rate-limit-remaining'])
